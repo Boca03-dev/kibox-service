c592ead814fb7f1ca9636d676b70a9e07664f9dc
Bogdan Bogicevic <bogicevicbogdan03gmail.com>
2026-03-18 16:21:57 +0100

    server.js

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// Routes (dodavacemo kasnije)
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+```
+
+Zatim otvori **.env** fajl i dodaj:
+```
+MONGO_URI=mongodb
+PORT=5000
+JWT_SECRET=kibox_secret_key
